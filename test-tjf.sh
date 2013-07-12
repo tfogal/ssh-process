8c02877fd9e4b769ae7843688b6b6c28e4e8dcb8
Tom Fogal <tfogal@alumni.unh.edu>
2013-07-12 17:44:40 +0200

    simple testing script

---
+++ test-tjf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+scp="./scp -S ./ssh"
+remote="grid16.ivda.uni-saarland.de"
+
+# -T: enable my stuff
+# -a: enable giving advice
+# -h: create 'holy' file.
+
+# simple test: just grabbing a small file
+${scp} -v -T ${remote}:DynamicBrickingDS.cpp .
+${scp} -v -T -a ${remote}:DynamicBrickingDS.cpp .
+${scp} -v -T -a -h ${remote}:DynamicBrickingDS.cpp .
